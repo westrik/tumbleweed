2e73917cc77ed75865f4f3663c42710446d608a1
Matt Westrik <m@ttwestrik.com>
2020-08-11 22:15:00 -0700

    sketch api

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,62 @@
+pub struct App {
+    pub name: String,
+    pub frontend_url: String,
+    pub backend_url: String,
+}
+
+// pub enum RequestMethod {
+//     DELETE,
+//     GET,
+//     HEAD,
+//     PATCH,
+//     POST,
+//     PUT,
+// }
+//
+// type ApiResult<A> = Result<A, String>;
+//
+// #[derive(Response)]
+// struct HelloResponse {
+//     pub message: String,
+// }
+//
+// #[endpoint("/", POST)]
+// async fn hello_world() -> ApiResult<HelloResponse> {
+//
+// }
+//
+// #[derive(Entity)]
+// struct Note {
+//     pub name: String,
+// }
+//
+// #[derive(Request)]
+// struct CreateNoteRequest {
+//     pub name: String,
+// }
+// #[derive(Response)]
+// struct CreateNoteResponse {
+//     pub note: Note,
+// }
+// #[endpoint("/note", [PUT, POST])]
+// async fn create_note(_request: CreateNoteRequest) -> ApiResult<CreateNoteResponse> {
+//     Ok(CreateNoteResponse {
+//         note: Note {
+//             name: "my note".to_string()
+//         }
+//     })
+// }
+//
 #[cfg(test)]
-mod tests {
+mod test_configuration {
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn app_creation() {
+        let app = App {
+            name: "Tumbleweed Test App".to_string(),
+            frontend_url: "https://www.example.com".to_string(),
+            backend_url: "https://api.example.com".to_string(),
+        };
     }
 }
