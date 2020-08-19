62b5dce91373e2edd01f14dde72caf587c00fcc5
Matt Westrik <m@ttwestrik.com>
2020-08-18 17:17:26 -0700

    add wasm_bindgen to exported structs

--- examples/tumbleweed_todos/todos/todos_core/src/lib.rs
+++ examples/tumbleweed_todos/todos/todos_core/src/lib.rs
@@ -1,10 +1,31 @@
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
 
 // TODO: generate structs from schema
 
+#[wasm_bindgen]
 #[derive(Serialize, Deserialize)]
 pub struct TestData {
-    pub message: String,
+    message: Option<String>,
+}
+
+impl TestData {
+    pub fn new() -> Self {
+        TestData { message: None }
+    }
+}
+
+#[wasm_bindgen]
+impl TestData {
+    #[wasm_bindgen(getter)]
+    pub fn message(&self) -> Option<String> {
+        self.message.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_message(&mut self, message: String) {
+        self.message = Some(message);
+    }
 }
 
 // #[cfg(test)]
