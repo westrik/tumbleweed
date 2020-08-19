c588e1838bbc14f4f63c28687a0988aba43cefd8
Matt Westrik <m@ttwestrik.com>
2020-08-18 23:38:27 -0700

    cargo fmt

--- examples/tumbleweed_todos/todos/src/lib.rs
+++ examples/tumbleweed_todos/todos/src/lib.rs
@@ -1,32 +1,47 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 // TODO: generate structs from schema
 
-#[wasm_bindgen]
+// #[wasm_bindgen]
 #[derive(Serialize, Deserialize)]
 pub struct TestData {
-    message: Option<String>,
+    pub message: String,
+    pub data: HashMap<String, String>,
 }
 
-impl TestData {
-    pub fn new() -> Self {
-        TestData { message: None }
-    }
-}
+// impl TestData {
+//     pub fn new() -> Self {
+//         TestData {
+//             message: None,
+//             data: None,
+//         }
+//     }
+// }
 
-#[wasm_bindgen]
-impl TestData {
-    #[wasm_bindgen(getter)]
-    pub fn message(&self) -> Option<String> {
-        self.message.clone()
-    }
-
-    #[wasm_bindgen(setter)]
-    pub fn set_message(&mut self, message: String) {
-        self.message = Some(message);
-    }
-}
+// #[wasm_bindgen]
+// impl TestData {
+//     #[wasm_bindgen(getter)]
+//     pub fn message(&self) -> Option<String> {
+//         self.message.clone()
+//     }
+//
+//     #[wasm_bindgen(setter)]
+//     pub fn set_message(&mut self, message: String) {
+//         self.message = Some(message);
+//     }
+//
+//     #[wasm_bindgen(getter)]
+//     pub fn data(&self) -> Option<HashMap<String, String>> {
+//         self.data.clone()
+//     }
+//
+//     #[wasm_bindgen(setter)]
+//     pub fn set_data(&mut self, data: HashMap<String, String>) {
+//         self.data = Some(data);
+//     }
+// }
 
 // #[cfg(test)]
 // mod tests {
